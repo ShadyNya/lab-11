9ce2b688a7354aa53827e8f51fb5731c74482e8a
ShadyNya <71332634+ShadyNya@users.noreply.github.com>
2020-11-27 10:12:25 +0300

    Add files via upload

---
+++ clua.lua
@@ -0,0 +1,13 @@
+
+function run ()
+
+	local a = 2	
+	local b = 4
+	local c = 1
+
+	local x1, x2 = solve_eq(a, b, c)
+
+	print (x1, x2)
+
+end
+-- Studentova_473
---
+++ clua.sh~
@@ -0,0 +1,4 @@
+
+rm -f app
+gcc -Wall -O3 clua.c -o app -llua5.3 -lm -I/usr/include/lua5.3
+./app
